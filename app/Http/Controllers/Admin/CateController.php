273b737988f540ac2262bacc3337bfb054399230
csyun <18612567218@qq.com>
2017-12-01 13:03:23 +0800

    修改code模目录

--- app/Http/Controllers/Admin/CateController.php
+++ app/Http/Controllers/Admin/CateController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Modles\Cate;
+use App\Models\Cate;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
